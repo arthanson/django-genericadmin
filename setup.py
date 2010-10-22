05110a49d2c7a8740726f5415dbf8cf04974f837
Jan Schrewe <jan@schafproductions.com>
2010-10-22 02:26:13 +0200

    * updated setup.py
    --HG--
    branch : django-genericadmin

--- setup.py
+++ setup.py
@@ -1,78 +1,15 @@
-import os, re
-from setuptools import setup, find_packages
 
-app_name = 'genericadmin'
-
-def get_svn_revision(path=None):
-    rev = None
-    entries_path = '%s/.svn/entries' % path
-
-    if os.path.exists(entries_path):
-        entries = open(entries_path, 'r').read()
-        # Versions >= 7 of the entries file are flat text.  The first line is
-        # the version number. The next set of digits after 'dir' is the revision.
-        if re.match('(\d+)', entries):
-            rev_match = re.search('\d+\s+dir\s+(\d+)', entries)
-            if rev_match:
-                rev = rev_match.groups()[0]
-        # Older XML versions of the file specify revision as an attribute of
-        # the first entries node.
-        else:
-            from xml.dom import minidom
-            dom = minidom.parse(entries_path)
-            rev = dom.getElementsByTagName('entry')[0].getAttribute('revision')
-
-    if rev:
-        return u'svn-r%s' % rev
-    return u'svn-unknown'
-
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if not root_dir:
-    root_dir = '.'
-
-src_dir = os.path.join(root_dir, app_name)
-pieces = fullsplit(root_dir)
-if pieces[-1] == '':
-    len_root_dir = len(pieces) - 1
-else:
-    len_root_dir = len(pieces)
-
-for dirpath, dirnames, filenames in os.walk(src_dir):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'):
-            del dirnames[i]
-    #if 'conf' in dirpath:
-    #    print dirpath
-    if '__init__.py' in filenames and not 'conf' in dirpath:
-        packages.append('.'.join(fullsplit(dirpath)[len_root_dir:]))
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+from distutils.core import setup
 
 setup(
-    name=app_name,
-    version=get_svn_revision(root_dir),
+    name='genericadmin',
+    version='0.2',
     description="Adds support for generic relations within Django's admin interface.",
-    author='Weston Nielson',
-    author_email='wnielson@gmail.com',
-    url='http://westonsnotes.blogspot.com/',
-    packages = packages,
-    data_files = data_files,
+    author='Weston Nielson, Jan Schrewe',
+    author_email='wnielson@gmail.com, jschrewe@googlemail.com',
+    url='http://bitbucket.org/knurpsburps/django-genericadmin/overview',
+    packages = ['genericadmin'],
+    package_data={'genericadmin': ['media/js/*.js']},
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
