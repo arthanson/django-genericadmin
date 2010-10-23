87f27c3b2704c957ed80f5c767f6cf5b14721d50
Jan Schrewe <jan@schafproductions.com>
2010-10-23 02:53:19 +0200

    * works now with grappelli
    --HG--
    branch : django-genericadmin

--- genericadmin/admin.py
+++ genericadmin/admin.py
@@ -3,11 +3,23 @@ from django.conf.urls.defaults import patterns, url
 from genericadmin.views import generic_lookup, get_generic_rel_list
 from django.conf import settings
 
-JS_PATH = settings.MEDIA_URL + getattr(settings, 'GENERICADMIN_JS', '/genericadmin/js/genericadmin.js') 
+JS_PATH = getattr(settings, 'GENERICADMIN_JS', 'genericadmin/js/') 
 
 class GenericAdminModelAdmin(admin.ModelAdmin):
     class Media:
-        js = (JS_PATH,)
+        js = ()
+
+    def __init__(self, model, admin_site):
+        self.grappelli = False
+        media = list(self.Media.js)
+        for app in settings.INSTALLED_APPS:
+            if app == 'grappelli':
+                media.append(JS_PATH + 'genericadmin-grappelli.js')
+                self.grappelli = True
+        if not self.grappelli:
+            media.append(JS_PATH + 'genericadmin.js')
+        self.Media.js = tuple(media)
+        super(GenericAdminModelAdmin, self).__init__(model, admin_site)
 
     def get_urls(self):
         base_urls = super(GenericAdminModelAdmin, self).get_urls()
